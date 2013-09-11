bb35bd1e03a7cb9084e880e639a80dd55b600dd6
dreamyguy <i@dreamyguy.com>
2013-09-11 21:56:26 +0200

    Introduce search engines and output them with angular

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,49 @@ function hw() {
 
 var omniAppModule = angular.module('omniApp', []);
 
-omniAppModule.controller('engineController', function($scope) {
-	var engine = {};
-	engine.message = 'That all you got?';
-	$scope.engine = engine;
+var engines = [
+    {
+        "name":"google",
+        "url":"https://www.google.com/#q="
+    },
+    {
+        "name":"bing",
+        "url":"http://www.bing.com/search?q="
+    },
+    {
+        "name":"twitter",
+        "url":"https://twitter.com/search?q="
+    },
+    {
+        "name":"youtube",
+        "url":"http://www.youtube.com/results?search_query="
+    },
+    {
+        "name":"vimeo",
+        "url":"https://vimeo.com/search?q="
+    },
+    {
+        "name":"flickr",
+        "url":"http://www.flickr.com/search/?q="
+    },
+    {
+        "name":"lastfm",
+        "url":"http://www.last.fm/search?q="
+    },
+    {
+        "name":"soundcloud",
+        "url":"https://soundcloud.com/search?q="
+    },
+    {
+        "name":"grooveshark",
+        "url":"http://grooveshark.com/#!/search?q="
+    },
+    {
+        "name":"stackoverflow",
+        "url":"http://stackoverflow.com/search?q="
+    }
+];
+
+omniAppModule.controller('enginesController', function($scope) {
+	$scope.engines = engines;
 });
